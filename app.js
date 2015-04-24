be50f6fb42753e13c9399d47d6ce7d64c3cfb4f3
Ben Wong <ben@simplepickup.com>
2015-04-23 17:37:58 -0700

    New sign-up page

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express'),
     app = express();
 
 app.get('/signup', function(req, res) {
-	var name = req.query.name;
-	res.send('User ' + name + ' has signed up\n');
+	
+	var email = req.query.email;
+	var pass = req.query.pass;
+	
+	res.send('Signed up with email ' + email + ' and password ' + pass + '\n');
+		
 });
 
 app.listen(2000);
