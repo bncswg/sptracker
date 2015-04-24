cfb3312a908577bd40341e7121e468989bffe750
Ben Wong <ben@simplepickup.com>
2015-04-24 11:23:42 -0700

    Basic login and signup

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-var express = require('express'),
-    app = express();
+var express = require('express');
+var app = express();
 
-app.get('/signup', function(req, res) {
+var accounts = {"default": "1234"};
+
+app.get('/process_signup', function(req, res) {
 	
 	var email = req.query.email;
 	var pass = req.query.pass;
 	
-	res.send('Signed up with email ' + email + ' and password ' + pass + '\n');
-		
+	accounts[email] = pass;
+	
+	console.log('Created account with email "' + email + '" and password "' + pass + '"');
+	console.log("Current accounts: " + JSON.stringify(accounts) + "\n");
+	
+	res.redirect('/login');
+});
+
+app.get('/signup', function(req, res) {
+
+	res.sendFile(__dirname + '/signup.html');
+});
+
+app.get('/process_login', function(req, res) {
+	
+	var email = req.query.email;
+	var pass = req.query.pass;
+	
+	/*
+	if (accounts.hasOwnProperty(email))
+		res.send('Account exists');
+	else
+		res.send('Account does not exist');
+	*/
+	
+	if (accounts[email] === pass)
+		res.send('Login successful');
+	else
+		res.send('Login failed');
+
+});
+
+app.get('/login', function(req, res) {
+	res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/', function(req, res) {
+	res.redirect('/login');
 });
 
 app.listen(2000);
