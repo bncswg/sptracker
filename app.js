88fdfa0b1fbe7f26efac351dcc964d63b2ac8958
Ben Wong <ben@simplepickup.com>
2015-04-27 10:42:29 -0700

    Created user and equipment schema

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
-var express = require('express');
+var express = require( 'express' );
 var app = express();
 var mongodb = require( 'mongodb' );
 var mongoose = require( 'mongoose' );
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
-var accounts = {"default": "1234"};
+var accounts = { "default": "1234" };
 
-var mongoose = require('mongoose');
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-
+mongoose.connect( 'mongodb://localhost/test' );
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  // yay!
-	console.log('successful');
+db.on( 'error', console.error.bind( console, 'connection error:' ) );
+
+db.once( 'open', function ( callback ){
+	var userSchema = mongoose.Schema({
+		email: String,
+		password: String,
+		equipments: [{ type: Number, ref: 'Equipment' }]
+	});
+	
+	var equipmentSchema = mongoose.Schema({
+		_id: Number,
+		name: String,
+	});
+	
+	var User = mongoose.model( 'User', userSchema );
+	var Equipment = mongoose.model( 'Equipment', equipmentSchema );
 });
 
-app.get('/process_signup', function(req, res) {
+app.get( '/process_signup', function( req, res ){
 	
 	var email = req.query.email;
 	var pass = req.query.pass;
 	
-	accounts[email] = pass;
+	accounts[ email ] = pass;
 	
-	console.log('Created account with email "' + email + '" and password "' + pass + '"');
-	console.log("Current accounts: " + JSON.stringify(accounts) + "\n");
+	console.log( 'Created account with email "' + email + '" and password "' + pass + '"' );
+	console.log( "Current accounts: " + JSON.stringify( accounts ) + "\n" );
 	
-	res.redirect('/login');
+	res.redirect( '/login' );
 });
 
-app.get('/signup', function(req, res) {
+app.get( '/signup', function( req, res ){
 
-	res.sendFile(__dirname + '/signup.html');
+	res.sendFile( __dirname + '/signup.html' );
 });
 
-app.get('/process_login', function(req, res) {
+app.get( '/process_login', function( req, res ){
 	
 	var email = req.query.email;
 	var pass = req.query.pass;
@@ -47,22 +56,22 @@ app.get('/process_login', function(req, res) {
 		res.send('Account does not exist');
 	*/
 	
-	if (accounts[email] === pass)
+	if ( accounts[ email ] === pass )
 		//res.send('Login successful');
-		res.sendFile(__dirname + '/inventory.html');
+		res.sendFile( __dirname + '/inventory.html' );
 	else
-		res.send('Login failed');
+		res.send( 'Login failed' );
 
 });
 
-app.get('/login', function(req, res) {
-	res.sendFile(__dirname + '/login.html');
+app.get( '/login', function( req, res ) {
+	res.sendFile( __dirname + '/login.html' );
 });
 
-app.get('/', function(req, res) {
-	res.redirect('/login');
+app.get( '/', function( req, res ) {
+	res.redirect( '/login' );
 });
 
-app.listen(2000);
+app.listen( 2000 );
 
-console.log('Express server started on port 2000');
+console.log( 'Express server started on port 2000' );
