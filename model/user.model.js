aa3a08613efe9c134a6a78e0a71ea4e3cf28109d
Ben Wong <ben@simplepickup.com>
2015-04-29 18:05:44 -0700

    Changes class methods to exported functions

--- model/user.model.js
+++ model/user.model.js
@@ -8,57 +8,54 @@ var userSchema = mongoose.Schema({
 	equipments: [{ type: Number, ref: 'Equipment' }]
 });
 
-userSchema.statics = {
-	/*
-	censor: function( ) {
-		delete this.password;
-		return this;
-	},
-	*/
-	
-	create: function( info, callback ) {
-		if ( info.name && info.email && info.password ) {
-			var user = new User( info );
-			user.save(function ( err, user ) {
-				if ( err ) return console.error( err );
-				callback( true );
-			});
-		} else {
-			callback( false );
-		}
-	},
-	
-	findByEmail: function( email, callback ) {
-		this.findOne( { email: email }, function( err, user ) {
-			if ( err ) return console.error( err );
-			callback( user );
-		});
-	},
-	
-	removeByEmail: function( email, callback ) {
-		this.findOneAndRemove({ email: email }, function( err, user ) {
-			if ( err ) return console.error( err );
-			if ( user )
-				callback( true );
-			else
-				callback( false );
-		});
-	},
-	
-	findAll: function( callback ) {
-		this.find( function( err, users ) {
-			if ( err ) return console.error( err );
-			callback( users );
-		});
-	},
-	
-	removeAll: function( callback ) {
-		this.remove( function( err ) {
-			if ( err ) return console.error( err );
-			callback();
-		});
-	}
-};
+/*
+censor: function( ) {
+	delete this.password;
+	return this;
+},
+*/
 
 var User = mongoose.model( 'User', userSchema );
-module.exports = User;
+
+exports.create = function create( info, callback ) {
+	if ( info.name && info.email && info.password ) {
+		var user = new User( info );
+		user.save(function ( err, user ) {
+			if ( err ) return console.error( err );
+			callback( true );
+		});
+	} else {
+		callback( false );
+	}
+};
+	
+exports.findByEmail = function findByEmail( email, callback ) {
+	User.findOne( { email: email }, function( err, user ) {
+		if ( err ) return console.error( err );
+		callback( user );
+	});
+};
+	
+exports.removeByEmail = function removeByEmail( email, callback ) {
+	User.findOneAndRemove({ email: email }, function( err, user ) {
+		if ( err ) return console.error( err );
+		if ( user )
+			callback( true );
+		else
+			callback( false );
+	});
+};
+	
+exports.findAll = function findAll( callback ) {
+	User.find( function( err, users ) {
+		if ( err ) return console.error( err );
+		callback( users );
+	});
+};
+	
+exports.removeAll =  function removeAll( callback ) {
+	User.remove( function( err ) {
+		if ( err ) return console.error( err );
+		callback();
+	});
+};
